ccb01c4eafde0061baff62eed91fed1241e690f3
adeoluwadavid <adeoluwadavid@gmail.com>
2020-10-07 23:40:55 +0100

    'Final-Commit'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     const { books } = useContext(BookContext)
     return (
         <div className="navbar">
-            <h1>Ninja Reading List</h1>
+            <h1>Adewole Reading List Application</h1>
             <p>Currently you have {books.length} books to get through...</p>
         </div>
     );
