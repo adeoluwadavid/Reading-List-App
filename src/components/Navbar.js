ee799b678835909636696d1a0f14edda49555eeb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-10 00:08:41 +0000

    Deploy adeoluwadavid/Reading-List-App to github.com/adeoluwadavid/Reading-List-App.git:gh-pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import { BookContext } from '../context/BookContext';
+
+const Navbar = () => {
+    const { books } = useContext(BookContext)
+    return (
+        <div className="navbar">
+            <h1>Adewole Book List Application</h1>
+            <p>Currently you have {books.length} books to get through...</p>
+        </div>
+    );
+}
+
+export default Navbar;
